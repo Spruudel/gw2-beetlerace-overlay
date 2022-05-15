317e60e1d767d4d1093812975bf7b01a5737eb85
Spruudel <25455600+Spruudel@users.noreply.github.com>
2022-05-15 17:03:54 +0200

    restructured files

---
+++ js/map_websocket.js
@@ -0,0 +1,33 @@
+const test = 5;
+
+function createMapWebocket(hostname, port, consumer) {
+    let socket = new WebSocket(`ws://${hostname}:${port}`);
+
+    socket.onopen = (e) => {
+        // send initial packet to be registered as consumer
+        const initEvent = {
+            type: "init",
+            client: "map",
+            room: room,
+        };
+        socket.send(JSON.stringify(initEvent));
+    };
+
+    socket.onmessage = ({ data }) => {
+        const event = JSON.parse(data);
+        switch (event.type) {
+            case "position":
+                consumer(event);
+                // console.info(event);
+                break;
+            default:
+                throw new Error(`Unsupported event type: ${event.type}.`);
+        }
+    };
+
+    socket.onerror = function (error) {
+        console.error(`[WebSocket] ${error.message}`);
+    };
+
+    return socket;
+}
